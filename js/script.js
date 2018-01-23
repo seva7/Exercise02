c6b27c93732eb375f8ba8c291741088b751b8651
SevanBelleau <sevanbelleau@gmail.com>
2018-01-23 16:38:55 -0500

    added animation on images and random math

--- js/script.js
+++ js/script.js
@@ -8,8 +8,21 @@ Description of what the script does...
 
 **********************************************/
 
-$(document).ready() {
-
-  // Insert jQuery code here to run when the page is loaded
-  
-}
+$(document).ready(function () {
+  $('img').each(function () {
+    var x = Math.random() * ($(window).width() - $(this).width());
+    var y = Math.random() * ($(window).height() - $(this).height());
+    $(this).css({
+      top: y,
+      left: x
+    });
+  });
+  $('img').on('mouseover',function () {
+    var a = Math.random() * 100;
+    var b = Math.random() * 100;
+    $(this).animate({
+      top: a,
+      left: b,
+    },500);
+  });
+});
